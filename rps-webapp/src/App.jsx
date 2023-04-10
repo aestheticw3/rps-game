f6f9cd86fb99788e545cf3d528ef11427b027279
Danila Kudashov <kudashovdanila@gmail.com>
2023-04-10 23:19:06 +0700

    webpack => vite

--- rps-webapp/src/App.jsx
+++ rps-webapp/src/App.jsx
@@ -1,6 +1,6 @@
 import styles from "./App.module.css";
-import Header from "./Header/Header";
-import Main from "./Main/Main";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 function App() {
   return (
     <div className="App" style={styles.title}>
